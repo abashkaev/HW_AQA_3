8ab0ad18cc1320f6baa5f47b842f068870bed432
AleksandrBashkaev <Bashkaev_a@mail.ru>
2023-06-22 19:06:31 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/dcpcb35lf1ii7e4s/branch/main?svg=true)](https://ci.appveyor.com/project/abashkaev/hw-aqa-3/branch/main)
---
+++ src/test/java/OrderCardTest.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class OrderCardTest {
+
+    @Test
+    void orderCardTest (){
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Игорь");
+        $("[data-test-id=phone] input").setValue("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(".button__content .button__text").click();
+
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        sleep(5000);
+    }
+}
